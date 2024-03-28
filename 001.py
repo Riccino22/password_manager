bbc4cdfe19f7a22aefc027bb4148d01676674207
Riccino <riccardorinojosam@gmail.com>
2024-03-28 11:13:50 -0300

    The password generator has already been created

--- 001.py
+++ 001.py
@@ -1,6 +1,32 @@
 import PySimpleGUI as sg
+import random
+import string
 
-label = sg.Text("")
+def generate_password():
+    char = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(char) for _ in range(random.randint(12,20)))
+    return password
 
-window = sg.Window("Password Manager", layout=[[label]])
-window.read()
+btn_create = sg.Button("New")
+input_text = sg.InputText(default_text=generate_password(), key="password")
+layout = [
+    [ btn_create ], 
+    [ input_text ]
+]
+
+print(input_text.DefaultText)
+window = sg.Window("Password Manager", layout=layout)
+
+
+while True:
+    event, values =  window.read()
+    if event == sg.WINDOW_CLOSED:
+        print("Bye")
+        break
+    else:
+        print(values)
+        new_password =  generate_password()
+        window["password"].update(new_password)
+        
+
+window.close()
